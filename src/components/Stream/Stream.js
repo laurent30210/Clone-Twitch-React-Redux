d44c103121be20a9909e3c3b2329754a5374ecec
laurent30210 <fligbook@yahoo.fr>
2021-04-02 15:51:05 +0200

    CREATE new componant

--- src/components/Stream/Stream.js
+++ src/components/Stream/Stream.js
@@ -1,17 +1,37 @@
-// == Import npm
-import React from 'react';
-// == Import
-import './channelStream.scss';
-import { changeTypeTextLanguage, changeTypeNumberOfViewers } from 'src/utils';
+import React, { useEffect, useRef } from 'react';
+import './stream.scss';
 import { Link } from 'react-router-dom';
 
-// == Composant
-const ChannelStream = ({
-  id, userName, gameName, title, viewer, language, image, loginName, userPic, type,
-}) =>  (
+const Stream = ({
+  id,
+  userName,
+  gameName,
+  title,
+  viewer,
+  language,
+  image,
+  loginName,
+  userPic,
+  type,
+}) => {
+  const backgroundRef = useRef(null);
+  const cornerTopRef = useRef(null);
+  const cornerRightRef = useRef(null);
+
+  useEffect(() => {
+    const color = colorsRandom();
+    backgroundRef.current.style.backgroundColor = color;
+    cornerTopRef.current.style.borderRight = `.4rem solid ${color}`;
+    cornerRightRef.current.style.borderTop = `.4rem solid ${color}`;
+  }, []);
+
+  return (
     <li className="channelStream" id={id}>
       <header className="channelStream__header">
-        <div className="backgroundImg" />
+        <div
+          ref={backgroundRef}
+          className="backgroundImg"
+        />
         <Link
           to={{
             pathname: `/live/${loginName}`,
@@ -26,8 +46,14 @@ const ChannelStream = ({
         </Link>
         <span className="channelStream__viewers">{`${changeTypeNumberOfViewers(viewer)} spectateurs`}</span>
         <span className="channelStream__live">{type}</span>
-        <div className="cornerTop" />
-        <div className="cornerRight" />
+        <div
+          className="cornerTop"
+          ref={cornerTopRef}
+        />
+        <div
+          className="cornerRight"
+          ref={cornerRightRef}
+        />
       </header>
       <section className="channelStream__containerUser">
         <img
@@ -42,17 +68,15 @@ const ChannelStream = ({
             }}
           >
             <h2 className="channelStream__containerUser__content__title">{title}</h2>
+            <h3 className="channelStream__containerUser__content__userName">{userName}</h3>
           </Link>
-          <h3 className="channelStream__containerUser__content__userName">{userName}</h3>
           <h3 className="channelStream__containerUser__content__gamePlayed">{gameName}</h3>
         </div>
       </section>
       <section className="channelStream__details">
         <span className="channelStream__details__icon channelStream__details__icon__language">{changeTypeTextLanguage(language)}</span>
-        <span className="channelStream__details__icon channelStream__details__icon__techno">réalité</span>
       </section>
     </li>
   );
-
-// == Export
-export default ChannelStream;
+};
+export default Stream;
